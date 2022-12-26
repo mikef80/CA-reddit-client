7f7288bc436f048256c7facce26e9e1a854b03f9
Mike <mike@mike-francis.org>
2022-12-26 22:51:38 +0000

    started building app - trying to use TDD! Currently doing the titleBar

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); */
+
+test('should render menu/title bar', () => {
+  render(<App />);
+  const titleBar = screen.getByRole('titleBar');
+  expect(titleBar).toBeInTheDocument();
+})
