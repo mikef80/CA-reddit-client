2bceb7f56651dafbc8dffd45f2d4eac8d6ffbb9c
Mike <mike@mike-francis.org>
2022-12-29 21:29:17 +0000

    working on fetch and statuses surrounding this.  Not helped by crappy hotel wifi!

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -1,22 +1,24 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { updatePosts } from './postsSlice';
+import { updatePosts, fetchPosts } from './postsSlice';
 
 const Posts = () => {
-    const posts = useSelector(state => state.posts.posts);
+    const posts = useSelector(state => state.posts);
+    let returnedPosts;
 
     const dispatch = useDispatch();
 
     const getPosts = async () => {
         // THINK I NEED A THUNK IN HERE OR POSTSSLICE TO UPDATE STATE ASYNC
-        const response = await fetch('https://www.reddit.com/search.json?q=surfing');
+        /* const response = await fetch('https://www.reddit.com/search.json?q=surfing');
         const raw = await response.json();
         console.log(raw.data.children[0].data.title);
         const posts = raw.data.children;
-        console.log(posts);
+        console.log(posts); */
 
-        dispatch(updatePosts(posts));
+        dispatch(fetchPosts());
+        // dispatch(updatePosts(posts));
     };
 
     useEffect(() => {
@@ -25,14 +27,29 @@ const Posts = () => {
 
     return (
         <div>
-            { posts.length ?
+            {/* { posts.isLoading ?
 
                 <p>Loading...</p>
                 :
-                posts.map(post => {
+                posts.posts.map(post => {
                     console.log(post);
-                    // return <p role="generic" key={ post.data.id }>{ post.data.title }</p>;
-                }) }
+                    return <p role="generic" key={ post.data.id }>{ post.data.title }</p>;
+                }) } */}
+            { posts.isLoading ?
+                <p>Loading...</p>
+                :
+                posts.posts.length > 0 ? 
+                posts.posts.map(post => {
+                    return post.map(item => {
+                        console.log(item);
+                        const { title, selftext } = item.data;
+                        console.log(`${title}: ${selftext}`);
+                        // return <p role="generic" key={ post.data.id }>{ post.data.title }</p>;
+                    })
+                })
+                    :
+                    <p>No posts returned.</p>
+            }
         </div>
     );
 };
