0580c7e4a4ef122c13a88f53d9f2c0ba83913a42
Mike <mike@mike-francis.org>
2023-01-05 19:42:40 +0000

    sync

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 
 const Post = (props) => {
-  const { title, thumbnail, selftext, media } = props.data;
-  
-  let video_url;
+    const { title, thumbnail, selftext, media } = props.data;
 
-  if (media) {
-    if(media.reddit_video) {
-      video_url = media.reddit_video.fallback_url;
+    let video_url;
+
+    if (media) {
+        if (media.reddit_video) {
+            video_url = media.reddit_video.fallback_url;
+        }
     }
-  }
 
-  return (
-    <div
-      role="generic"
-      className="border border-slate-400 rounded flex flex-col p-4 my-2 font"
-    >
-      <h3 className="ibmPlexSans font-medium pb-4">{title ? title : ""}</h3>
-      {!media && thumbnail ? <img src={thumbnail} alt={title} className='rounded-md' /> : ""}
-      {media && <video src={video_url} controls className="rounded-md" />}
-      <p className="notoSans font-normal text-sm">{selftext ? selftext : ""}</p>
-    </div>
-  );
+    return (
+        <div className="border border-slate-400 rounded flex flex-col p-4 my-2 font">
+            <h3 className="ibmPlexSans font-medium pb-4">{ title ? title : "" }</h3>
+            { !media && thumbnail ? <img src={ thumbnail } alt={ title } className='rounded-md' /> : "" }
+            { media && <video src={ video_url } controls className="rounded-md" /> }
+            <p className="notoSans font-normal text-sm">{ selftext ? selftext : "" }</p>
+        </div>
+    );
 };
 
 export default Post;
