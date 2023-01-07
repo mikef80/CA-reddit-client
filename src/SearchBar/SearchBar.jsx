3c53d9d5591e1c0e9b25e550ac53c34a01a87297
Mike <mike@mike-francis.org>
2023-01-07 19:58:40 +0000

    sync

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,29 +1,64 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { updateTerm } from "./searchBarSlice";
+import { updateTerm, cleanTerm } from "./searchBarSlice";
 import { fetchPosts, cleanPosts } from "../Posts/postsSlice";
+
+// export const SearchBar = () => {
+//   const term = useSelector((state) => state.searchBar.term);
+
+//   const dispatch = useDispatch();
+
+//   const updateInput = (e) => {
+//     console.log("update Input");
+//     const searchTerm = e.target.value;
+//     dispatch(updateTerm(searchTerm));
+//   };
+
+//   useEffect(() => {
+//     console.log(term);
+//     dispatch(fetchPosts(term));
+
+//     return () => {
+//       // clean up here
+//       dispatch(cleanTerm())
+//     };
+//   }, [term]);
+
+//   return (
+//     <div>
+//       <input
+//         type="search"
+//         name="search"
+//         id="search"
+//         className="border-black border-[1px] bg-slate-200 focus:outline-black focus:outline focus:outline-[1.5px] pl-1"
+//         role="search"
+//         onChange={updateInput}
+//         term={term}
+//         placeholder="Search..."
+//       />
+//       <p>{term}</p>
+//     </div>
+//   );
+// };
+
 
 export const SearchBar = () => {
   const term = useSelector((state) => state.searchBar.term);
 
   const dispatch = useDispatch();
 
-  const updateInput = (e) => {
-    console.log("update Input");
-    const searchTerm = e.target.value;
-    dispatch(updateTerm(searchTerm));
-  };
+  const updateSearchTerm = e => {
+    dispatch(updateTerm(e.target.value));
+  }
 
   useEffect(() => {
-    console.log(term);
-    dispatch(fetchPosts(term));
+    const search = setTimeout(() => {
+      dispatch(fetchPosts(term));
+    },1000);
 
-    return () => {
-      // clean up here
-      dispatch(cleanPosts())
-    };
-  }, [term]);
+    return () => clearTimeout(search);
+  },[term])
 
   return (
     <div>
@@ -33,7 +68,7 @@ export const SearchBar = () => {
         id="search"
         className="border-black border-[1px] bg-slate-200 focus:outline-black focus:outline focus:outline-[1.5px] pl-1"
         role="search"
-        onChange={updateInput}
+        onChange={updateSearchTerm}
         term={term}
         placeholder="Search..."
       />
--- src/SearchBar/searchBarSlice.jsx
+++ src/SearchBar/searchBarSlice.jsx
@@ -9,10 +9,14 @@ export const searchBarSlice = createSlice({
     reducers: {
         updateTerm: (state, action) => {
             state.term = action.payload;
+        },
+        cleanTerm: (state, action) => {
+            state.term = '';
+            console.log('cleared search term');
         }
     }
 });
 
-export const { updateTerm } = searchBarSlice.actions;
+export const { updateTerm, cleanTerm } = searchBarSlice.actions;
 
 export default searchBarSlice.reducer;
